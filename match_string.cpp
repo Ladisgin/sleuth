cb2533b6a6839c71fbb0d8c6bd0ea9f71e03cbe0
kalugin <kalugin.vlad@gmail.com>
2019-01-19 05:48:23 +0300

    add match string

---
+++ match_string.cpp
@@ -0,0 +1,6 @@
+#include "match_string.h"
+
+match_string::match_string()
+{
+
+}
---
+++ match_string.h
@@ -0,0 +1,12 @@
+#ifndef MATCH_STRING_H
+#define MATCH_STRING_H
+
+#include <QObject>
+
+class match_string
+{
+public:
+    match_string();
+};
+
+#endif // MATCH_STRING_H
